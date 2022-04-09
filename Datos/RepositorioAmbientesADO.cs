198659165900f80a7c8286e843ff4e30ba7c3572
ehhugo <borgiani.hugo@gmail.com>
2022-04-08 22:34:35 -0300

    * Repositorio agregados e implementados:
    - RepositorioAmbientesADO
    - RepositorioIluminacionADO
    
    * Se eliminaron los repositorios correspondientes en memoria y el RepositorioTiposMemoria. El repositorio de plantas en memoria se dejó porque no tengo puta idea de como implementarlo en ADO
    
    * Falta agregar e implementar el repositorio de usuarios.

---
+++ Datos/RepositorioAmbientesADO.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+using Dominio.Entidades;
+using Dominio.InterfacesRepositorio;
+
+using Microsoft.Data.SqlClient;
+
+namespace Datos
+{
+    class RepositorioAmbientesADO : IRepositorio<Ambiente>
+    {
+
+        #region Add/Delete
+        public bool Add(Ambiente obj)
+        {
+            bool altaOK = false;
+
+            if (obj != null && obj.Validar() && FindById(obj.IdAmbiente) == null)
+            {
+                SqlConnection con = Conexion.ObtenerConexion();
+                string sql = "INSERT INTO Ambientes VALUES (@TipoAmbiente); SELECT CAST (SCOPE_IDENTITY() AS INT);";
+                SqlCommand SQLcom = new SqlCommand(sql, con);
+                SQLcom.Parameters.AddWithValue("@TipoAmbiente", obj.TipoAmbiente);
+
+                try
+                {
+                    Conexion.AbrirConexion(con);
+                    int id = (int)SQLcom.ExecuteScalar();
+                    obj.IdAmbiente = id;
+                    altaOK = true;
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    Conexion.CerrarYTerminarConexion(con);
+                }
+            }
+            return altaOK;
+        }
+        public bool Remove(int id)
+        {
+            bool eliminarOK = false;
+            SqlConnection con = Conexion.ObtenerConexion();
+            string sql = $"DELETE FROM Ambientes WHERE idAmbiente = {id};";
+            SqlCommand SQLCom = new SqlCommand(sql, con);
+
+            try
+            {
+                Conexion.AbrirConexion(con);
+                int filasAfectadas = SQLCom.ExecuteNonQuery();
+                eliminarOK = filasAfectadas == 1;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Conexion.CerrarYTerminarConexion(con);
+            }
+            return eliminarOK;
+        }
+
+        #endregion
+
+        #region Find
+        public IEnumerable<Ambiente> FindAll()
+        {
+            List<Ambiente> ambientes = new List<Ambiente>();
+
+            SqlConnection con = Conexion.ObtenerConexion();
+
+            string sql = "SELECT * FROM Ambientes;";
+            SqlCommand SQLCom = new SqlCommand(sql, con);
+
+            try
+            {
+                Conexion.AbrirConexion(con);
+                SqlDataReader reader = SQLCom.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Ambiente a = new Ambiente()
+                    {
+                        IdAmbiente = reader.GetInt32(reader.GetOrdinal("idAmbiente")),
+                        TipoAmbiente = reader.GetString(1),
+                    };
+                    ambientes.Add(a);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Conexion.CerrarYTerminarConexion(con);
+            }
+            return ambientes;
+        }
+
+        public Ambiente FindById(int id)
+        {
+            Ambiente buscado = null;
+
+            SqlConnection con = Conexion.ObtenerConexion();
+
+            string sql = $"SELECT * FROM Ambiente WHERE idAmbiente = {id};";
+            SqlCommand SQLCom = new SqlCommand(sql, con);
+
+            try
+            {
+                Conexion.AbrirConexion(con);
+                SqlDataReader reader = SQLCom.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    buscado = new Ambiente()
+                    {
+                        IdAmbiente = reader.GetInt32(reader.GetOrdinal("idAmbiente")),
+                        TipoAmbiente = reader.GetString(1),
+                    };
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                Conexion.CerrarYTerminarConexion(con);
+            }
+
+            return buscado;
+        }
+        #endregion
+
+        #region Update
+        public bool update(Ambiente obj)
+        {
+            bool ok = false;
+            SqlConnection con = Conexion.ObtenerConexion();
+
+            if (obj.Validar())
+            {
+                string sql = "UPDATE Ambientes SET TipoAmbiente = @TipoAmbinete WHERE idAmbiente =@IdAmbiente";
+                SqlCommand SQLCom = new SqlCommand(sql, con);
+
+                SQLCom.Parameters.AddWithValue("@TipoAmbinete", obj.TipoAmbiente);
+                SQLCom.Parameters.AddWithValue("@IdAmbiente", obj.IdAmbiente);
+
+                try
+                {
+                    Conexion.AbrirConexion(con);
+                    int afectadas = SQLCom.ExecuteNonQuery();
+                    ok = afectadas == 1;
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    Conexion.CerrarYTerminarConexion(con);
+                }
+            }
+            return ok;
+        }
+        #endregion
+    }
+}
--- Dominio/Entidades/Ambiente.cs
+++ Dominio/Entidades/Ambiente.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dominio.InterfacesRepositorio;
 
 namespace Dominio.Entidades
 {
-    public class Ambiente
-    {
+    public class Ambiente { 
 
         public int IdAmbiente { get; set; }
 
-        public string TipoAmiente { get; set; }
+        public string TipoAmbiente { get; set; }
+
+        public bool Validar()
+        {
+            if (TipoAmbiente != null)
+            {
+                if (TipoAmbiente.Trim().Length > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
